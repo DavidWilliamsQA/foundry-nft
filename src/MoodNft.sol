aaebf93234acd7a5335f2918d29762eff8e7a6c9
David Williams <temiloluwaw@gmail.com>
2024-11-26 17:30:22 +0000

    nft project commit

---
+++ src/MoodNft.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    error MoodNft__CantFlipMoodIfNotOwner();
+    uint256 private s_tokenCounter;
+    string private s_happySvgImageUri;
+    string private s_sadSvgImageUri;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory happySvgImageUri,
+        string memory sadSvgImageUri
+    ) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_happySvgImageUri = happySvgImageUri;
+        s_sadSvgImageUri = sadSvgImageUri;
+    }
+
+    function mintNft() public {
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        if (
+            getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender
+        ) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageUri;
+
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageUri = s_happySvgImageUri;
+        } else {
+            imageUri = s_sadSvgImageUri;
+        }
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                                '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                                imageUri,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+}
